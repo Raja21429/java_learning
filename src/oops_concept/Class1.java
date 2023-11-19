df532cc4d7b78e7b17a534e3822537396b5342fa
Raja21429 <rr912816@gmail.com>
2023-11-19 14:06:51 +0530

    Encapsulation

---
+++ src/oops_concept/Class1.java
@@ -0,0 +1,33 @@
+package oops_concept;
+
+public class Class1 {
+
+	/*public void text(){
+		
+			float f=40000;
+			return;
+			
+				
+			
+	}
+	public void text1(){
+		int bonus=20000;
+		
+		
+		
+		return;
+	}
+	public void text2(){
+		
+	}
+	public void text3(){
+		
+	}*/
+	public void eat() {
+		System.out.println("Welcome");
+	}
+	
+		
+	}
+
+
---
+++ src/oops_concept/Class2.java
@@ -0,0 +1,22 @@
+package oops_concept;
+
+public class  Class2 {
+	
+
+	/*public void text4() {
+		
+	}
+	public void text5() {
+			
+		}
+	public void text6() {
+		
+	}*/
+	
+	//public void eat(){
+		//System.out.println("Thank You");
+	
+	
+			
+	}
+}
